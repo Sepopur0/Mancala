2539c4f0763e2fed823e61f62719cc92acc05dc8
loc.1011 <loc.le1011@hcmut.edu.vn>
2022-05-05 23:28:12 +0700

    Update UX/UI

--- data.py
+++ data.py
@@ -16,36 +16,38 @@ INT_MIN = np.iinfo(np.int32).min
 # buttoncoordinate(left,top)
 startbutton_coor = [(575, 300), (575,375), (575, 450)]
 gamebutton_coor=[[475,545],[675,545]]
-option_coor_0=[(375,500),(775,500),(775,550)]
+option_coor_0=[(375,500),(775,500),(775,575)]
 direct_coor=[]
-option_coor_1=[(375,500),(375,575),(775,500),(775,550)]
+option_coor_1=[(375,500),(375,575),(775,500),(775,575)]
 board_coor=[(930,270),(815,325),(705,325),(595,325),(485,325),
             (375,325),(270,270),(375,215),(485,215),(595,215),(705,215),(815,215)]
 # font
 font = pygame.font.SysFont('calibri', 25, True, False)
 font_number = pygame.font.SysFont('calibri', 30, True, False)
-font_title = pygame.font.SysFont('opensansregular',60,True,True)
+font_title = pygame.font.SysFont('opensansregular',100,True,True)
 # colors
 black = (0, 0, 0)
 white = (255, 255, 255)
-lime_green = (120,190,33)
-red_1 = (128,5,0)
-yellow = (255, 255, 0)
-gray = (128, 128, 128)
-rock=(90,77,65)
-charcoal=(54,69,79)
+boxcolor = (244, 199, 171) #mau o
+scenery =(134, 84, 57)   #nen
+shading = (215, 177, 157) #clickon
+boxcontent= (64,34,24) #mau chu va mau da
+rock_1=(128, 128, 128)
+rock_2=(90,77,65)
+rock=(54,69,79)
 red=(139,0,0)
 green=(0,139,0)
 blue=(0,0,139)
-earth_brown=(129,97,62)
+playzonecolor=(198,139,89)
+#boxcontent=(64,34,24) #mau chu
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Mancala")
+pygame.display.set_caption("Ô ăn quan")
 s_1 = pygame.Surface((150, 40))
-s_1.fill(earth_brown)
-pygame.draw.rect(s_1, yellow, pygame.Rect(
+s_1.fill(scenery)
+pygame.draw.rect(s_1, shading, pygame.Rect(
         0, 0, button_width, button_height), 0, 40)
 pygame.Surface.set_alpha(s_1, 100)
 s=pygame.Surface((110,110))
-s.fill(yellow)
+s.fill(shading)
 pygame.Surface.set_alpha(s,100)
--- funcs.py
+++ funcs.py
@@ -1,12 +1,13 @@
-from multiprocessing.pool import INIT
 import data 
 from agents import *
 def start_screen(screen): #0 for p1 vs p2; 1 for p1 vs bot
-    screen.fill(earth_brown)
-    bg_img = pygame.image.load('download.jpg')
+    screen.fill(scenery)
+    bg_img = pygame.image.load('download.jpeg')
     bg_img = pygame.transform.scale(bg_img,(screen_width,screen_height))
     screen.blit(bg_img,(0,0))
-    screen.blit(font_title.render("Ô ăn loz",False,red_1),(500,100))
+    text=font_title.render("Ô ăn quan",False,boxcontent)
+    tet_coor = text.get_rect(center=(650, 200))
+    screen.blit(text,tet_coor)
     drawbutton(screen,"P1 vs. P2", 575, 300)
     drawbutton(screen,"P1 vs. Agent", 575, 375)
     drawbutton(screen,"Thoát", 575, 450)
@@ -32,8 +33,12 @@ def start_screen(screen): #0 for p1 vs p2; 1 for p1 vs bot
         pygame.display.update()
     
 def options(screen,opt): # 0 for p1 first, 1 for p2/bot first; if play w/ bot: return num as lv, -1 if play w/ human 
-    screen.fill(earth_brown)
-    screen.blit(font.render("Tùy chọn",False,red_1),(385,475))
+    screen.fill(rock_1)
+    bg_img = pygame.image.load('download.jpeg')
+    bg_img = pygame.transform.scale(bg_img,(screen_width,screen_height))
+    bg_img.set_alpha(40)
+    screen.blit(bg_img,(0,0))
+    screen.blit(font.render("Tùy chọn",False,boxcontent),(385,475))
     option=""
     drawbutton(screen,"P1 trước", 375, 500)
     coor=[]
@@ -43,11 +48,11 @@ def options(screen,opt): # 0 for p1 first, 1 for p2/bot first; if play w/ bot: r
     else:
         option="Agent trước"
         coor=option_coor_1
-        screen.blit(font.render("Độ khó",False,red_1),(385,550))
+        screen.blit(font.render("Độ khó",False,boxcontent),(385,550))
         drawbutton(screen,"Dễ",375,575)
     draw_grid(screen)   
     drawbutton(screen,"Xác nhận",775,500)
-    drawbutton(screen,"Trang chính", 775, 550)
+    drawbutton(screen,"Trang chính", 775, 575)
     pygame.display.update()
     act = -1
     botdif=0
@@ -91,7 +96,11 @@ def options(screen,opt): # 0 for p1 first, 1 for p2/bot first; if play w/ bot: r
     
     
 def everything(screen,numofps,opt,opt2):#opt is for whofirst; opt2 is difficulty
-    screen.fill(earth_brown)
+    screen.fill(scenery)
+    bg_img = pygame.image.load('download.jpeg')
+    bg_img = pygame.transform.scale(bg_img,(screen_width,screen_height))
+    bg_img.set_alpha(40)
+    screen.blit(bg_img,(0,0))
     BOARD=np.full(12,5)
     state= GameState(board=BOARD)
     agent=Agent()
@@ -104,10 +113,11 @@ def everything(screen,numofps,opt,opt2):#opt is for whofirst; opt2 is difficulty
         agent=MinimaxAgent(gstate=state,reversed=True,dept=2)
     elif opt2==2:
         agent=AlphaBetaAgent(gstate=state,reversed=True,dept=6)
-    draw_state(screen,state,playturn,numofps)
+    draw_state(screen,state,playturn,numofps,opt2)
     pygame.display.update()
     pau=False
     cell=-1
+    act=-1
     while True:
         for ev in pygame.event.get():
             if ev.type==MOUSEBUTTONDOWN:
@@ -138,13 +148,13 @@ def everything(screen,numofps,opt,opt2):#opt is for whofirst; opt2 is difficulty
                                 direct=-1*(1-playturn)
                             change_board_coor(screen,8,True)
                             point=GamePointer(cell,direct)
-                            res=perform_action(state,point,playturn,numofps)
+                            res=perform_action(state,point,playturn,numofps,opt2)
                             state=res
                             re=check_end(state,numofps)
                             if re!=-99:
                                 return re    
                             playturn=(numofps+playturn)%(numofps*2)
-                            draw_state(screen,state,playturn,numofps)
+                            draw_state(screen,state,playturn,numofps,opt2)
                         init_human=False
                     elif ((act in range(3,8) and playturn==0) or (act in range(9,14) and playturn==2)) and state.board[act-2]!=0:
                         change_board_coor(screen,act,False)
@@ -159,24 +169,25 @@ def everything(screen,numofps,opt,opt2):#opt is for whofirst; opt2 is difficulty
                     else:
                         drawbutton(screen, "Tạm dừng", 475, 545)
                         screen.blit(font.render("Trò chơi tạm dừng",
-                                    False, earth_brown), (600, 590))
+                                    False, scenery), (600, 590))
                 elif act==1:
                     return -2
             elif ev.type==pygame.QUIT:
                 sys.exit()      
         if playturn==1:
+            pygame.time.delay(500)
             point=agent.find_best_move()[0]
             is_upside = (point.id in range (1,6))
             if state.no_more_moves(is_upside):
                 state.scatter_stones(is_upside)
-                draw_state(screen,state,playturn,numofps)
+                draw_state(screen,state,playturn,numofps,opt2)
             pygame.display.update()
-            state=perform_action(state,point,playturn,numofps)
+            state=perform_action(state,point,playturn,numofps,opt2)
             re=check_end(state,numofps)
             if re!=-99:
                 return re
             playturn=(numofps+playturn)%(numofps+1)
-            draw_state(screen,state,playturn,numofps)
+            draw_state(screen,state,playturn,numofps,opt2)
         is_upside = (playturn==0)
         if state.no_more_moves(is_upside):
             state.scatter_stones(is_upside)
@@ -188,10 +199,10 @@ def everything(screen,numofps,opt,opt2):#opt is for whofirst; opt2 is difficulty
                     screen.blit(s,coor)
             pygame.display.update()
             pygame.time.delay(300)
-            draw_state(screen,state,playturn,numofps)
+            draw_state(screen,state,playturn,numofps,opt2)
         pygame.display.update()
 
-def perform_action(state:GameState, pointer: GamePointer,playturn,numofps):
+def perform_action(state:GameState, pointer: GamePointer,playturn,numofps,opt2):
         is_upside = (pointer.id in range (1,6))
         score = 0
         is_continue = True
@@ -200,7 +211,7 @@ def perform_action(state:GameState, pointer: GamePointer,playturn,numofps):
         while is_continue:
             stones = state.board[pointer.id]
             state.board[pointer.id] = 0
-            draw_state(screen,state,playturn,numofps)
+            draw_state(screen,state,playturn,numofps,opt2)
             if pointer.id in range(6,12):
                 draw_pile(screen,stones,(board_coor[pointer.id][0],board_coor[pointer.id][1]-110),-1)
             else:
@@ -212,7 +223,7 @@ def perform_action(state:GameState, pointer: GamePointer,playturn,numofps):
                     pointer.next()
                     state.board[pointer.id] += 1
                     stones -= 1
-                    draw_state(screen,state,playturn,numofps)
+                    draw_state(screen,state,playturn,numofps,opt2)
                     if pointer.id in range(6,12):
                         draw_pile(screen,stones,(board_coor[pointer.id][0],board_coor[pointer.id][1]-110),-1)
                     else:
@@ -233,7 +244,7 @@ def perform_action(state:GameState, pointer: GamePointer,playturn,numofps):
                                 screen.blit(font.render("Trò chơi tạm dừng",False, black), (600, 590))
                             else:
                                 drawbutton(screen, "Tạm dừng", 475, 545)
-                                screen.blit(font.render("Trò chơi tạm dừng",False, earth_brown), (600, 590))
+                                screen.blit(font.render("Trò chơi tạm dừng",False, scenery), (600, 590))
                         elif act==1:
                             pygame.event.clear()
                             state.call=-99
@@ -258,7 +269,7 @@ def perform_action(state:GameState, pointer: GamePointer,playturn,numofps):
                         screen.blit(s,(board_coor[pointer.id]))
                         pygame.display.update()
                         pygame.time.delay(500)
-                        draw_state(screen,state,playturn,numofps)
+                        draw_state(screen,state,playturn,numofps,opt2)
                         pygame.display.update()
                         pygame.time.delay(500)
                         pointer.next()
@@ -271,7 +282,7 @@ def perform_action(state:GameState, pointer: GamePointer,playturn,numofps):
             state.player1_score += score
         else:
             state.player2_score += score
-        draw_state(screen,state,playturn,numofps)
+        draw_state(screen,state,playturn,numofps,opt2)
         pointer.next()
         pygame.event.clear()
         return state              
@@ -298,9 +309,9 @@ def check_end(state: GameState,numofps):
     return -99
 
 def change_board_coor(screen,act,option=False):
-    color=red_1
+    color=boxcontent
     if option:
-        color=earth_brown
+        color=scenery
     if act<8:
         data.direct_coor.append([board_coor[act-2][0],board_coor[act-2][1]+121])
         data.direct_coor.append([board_coor[act-2][0]+66,board_coor[act-2][1]+121])
@@ -315,11 +326,13 @@ def change_board_coor(screen,act,option=False):
         data.direct_coor.clear()
 
 def drawbutton(screen, str, left, top):
-    pygame.draw.rect(screen, lime_green, pygame.Rect(
+    tet=font.render(str, False, boxcontent)
+    text_rect = tet.get_rect(center=(75, 20))
+    pygame.draw.rect(screen, boxcolor, pygame.Rect(
         left, top, button_width, button_height), 0, 40)
-    pygame.draw.rect(screen, red_1, pygame.Rect(
+    pygame.draw.rect(screen, boxcontent, pygame.Rect(
         left,top, button_width, button_height), 3, 40)
-    screen.blit(font.render(str, False, red_1), (left+15, top+10))  
+    screen.blit(tet,(text_rect[0]+left,text_rect[1]+top))  
     
 def checkbox(coor, coor_list,indent1,indent2):
     i = 0
@@ -330,20 +343,33 @@ def checkbox(coor, coor_list,indent1,indent2):
     return -1
 
 def draw_grid(screen):
-    pygame.draw.rect(screen, white, pygame.Rect(
+    pygame.draw.rect(screen, playzonecolor, pygame.Rect(
+        265, 215, 770,220 ),width=0,border_radius= 100)
+    pygame.draw.rect(screen, shading, pygame.Rect(
         265, 215, 770,220 ), 5, 100)
     for i in range (0,6):
-        pygame.draw.line(screen,white,(375+110*i,215),(375+110*i,435),5)
-    pygame.draw.line(screen,white,(375,325),(925,325),5)
+        pygame.draw.line(screen,shading,(375+110*i,215),(375+110*i,433),5)
+    pygame.draw.line(screen,shading,(375,325),(925,325),5)
 
-def draw_state(screen,state: GameState,playturn,numofps):
-    screen.fill(earth_brown)
+def draw_state(screen,state: GameState,playturn,numofps,opt2):
+    screen.fill(rock_1)
+    bg_img = pygame.image.load('download.jpeg')
+    bg_img = pygame.transform.scale(bg_img,(screen_width,screen_height))
+    bg_img.set_alpha(40)
+    screen.blit(bg_img,(0,0))
     draw_grid(screen)
     drawbutton(screen,"Trang chính",675,545)
     drawbutton(screen,"Tạm dừng",475,545)
     option=""
     if numofps==1:
-        option="Agent"
+        st=""
+        if opt2==0:
+            st="Easy"
+        elif opt2==1:
+            st="Intermediate"
+        else:
+            st="Hard"
+        option="Agent " + st
     else:
         option="Người chơi 2"
     for i in range(0,12):
@@ -354,8 +380,8 @@ def draw_state(screen,state: GameState,playturn,numofps):
             draw_pile(screen,state.board[i],board_coor[i])
     pygame.draw.rect(screen,white,(1100,405,185,140),5)
     pygame.draw.rect(screen,white,(25,105,185,140),5)
-    screen.blit(font_number.render("Người chơi 1",False,white),(1135,365))
-    screen.blit(font_number.render(option,False,white),(60,65))
+    screen.blit(font_number.render("Người chơi 1",False,white),(1105,365))
+    screen.blit(font_number.render(option,False,white),(30,65))
     screen.blit(font_number.render("Điểm: "+ str(state.player1_score),False,white),(1125,440))
     screen.blit(font_number.render("Nợ: "+ str(state.player1_debt),False,white),(1125,490))
     screen.blit(font_number.render("Điểm: "+ str(state.player2_score),False,white),(50,140))
@@ -365,39 +391,29 @@ def draw_state(screen,state: GameState,playturn,numofps):
     else:
         pygame.draw.polygon(screen,white,((230,175),(275,150),(275,200)))
         
-def draw_rock(screen,coor,i,j,code=True,special=False):#code is for color option; special is for big rock option
+def draw_rock(screen,coor,i,j,special=False):#code is for color option; special is for big rock option
     x=coor[0]
     y=coor[1]
     color=()
-    if code or special:
-        if j==0:
-            color=rock
-        elif j==1:
-            color=charcoal
-        else:
-            color=gray
+    if j==0:
+        color=rock
+    elif j==1:
+        color=rock_2
     else:
-        if j==0:
-            color=red
-        elif j==1:
-            color=blue
-        else:
-            color=green
+        color=rock_1
     if special==False:
         if i==0:
-            pygame.draw.polygon(screen,color,((x+5,y+30),(x+5,y+8),(x+10,y+5),(x+28,y+5),(x+25,y+20),(x+20,y+20)))
-            pygame.draw.polygon(screen,black,((x+5,y+30),(x+5,y+8),(x+10,y+5),(x+28,y+5),(x+25,y+20),(x+20,y+20)),1)
+            pygame.draw.ellipse(screen,color,(x+2,y+2,20,25))
+            pygame.draw.ellipse(screen,black,(x+2,y+2,20,25),1)
         elif i==1:
-            pygame.draw.polygon(screen,color,((x+5,y+10),(x+5,y+22),(x+10,y+23),(x+20,y+25),(x+25,y+19),(x+24,y+5),(x+22,y+5),(x+6,y+5)))
-            pygame.draw.polygon(screen,black,((x+5,y+10),(x+5,y+22),(x+10,y+23),(x+20,y+25),(x+25,y+19),(x+24,y+5),(x+22,y+5),(x+6,y+5)),1)
+            pygame.draw.ellipse(screen,color,(x+7,y+5,26,21))
+            pygame.draw.ellipse(screen,black,(x+7,y+5,26,21),1)
         elif i==2:
-            pygame.draw.polygon(screen,color,((x+10,y+9),(x+23,y+14),(x+23,y+23),(x+7,y+25)))
-            pygame.draw.polygon(screen,black,((x+10,y+9),(x+23,y+14),(x+23,y+23),(x+7,y+25)),1)
+            pygame.draw.ellipse(screen,color,(x+2,y+5,24,30))
+            pygame.draw.ellipse(screen,black,(x+2,y+5,24,30),1)
     else:
-        pygame.draw.polygon(screen,color,((x+34,y+5),(x+41,y+7),(x+62,y+35),(x+61,y+62),(x+46,y+80),(x+34,y+80),(x+25,y+67),(x+25,y+38)))
-        pygame.draw.polygon(screen,black,((x+34,y+5),(x+41,y+7),(x+62,y+35),(x+61,y+62),(x+46,y+80),(x+34,y+80),(x+25,y+67),(x+25,y+38)),1)
-    
-
+        pygame.draw.ellipse(screen,color,(x+22,y+2,50,85))
+        pygame.draw.ellipse(screen,black,(x+22,y+2,50,85),1)
 
 def draw_pile(screen,num,pos,special=1):
     x=pos[0]
@@ -412,7 +428,7 @@ def draw_pile(screen,num,pos,special=1):
         draw_rock(screen,(x+8,y+25),0,1)
         draw_rock(screen,(x+63,y+30),2,1) 
     elif num==3:
-        draw_rock(screen,(x+8,y+45),0,1)
+        draw_rock(screen,(x+28,y+45),0,1)
         draw_rock(screen,(x+72,y+43),2,1)
         draw_rock(screen,(x+46,y+7),1,2)
     elif num==4:
@@ -422,28 +438,28 @@ def draw_pile(screen,num,pos,special=1):
         draw_rock(screen,(x+69,y+5),2,2)
     elif num==5:
         draw_rock(screen,(x+10,y+47),0,1)
-        draw_rock(screen,(x+71,y+45),2,1)
-        draw_rock(screen,(x+12,y+7),1,2)
-        draw_rock(screen,(x+69,y+5),2,2)
-        draw_rock(screen,(x+45,y+25),0,2)
+        draw_rock(screen,(x+61,y+52),2,1)
+        draw_rock(screen,(x+12,y+17),1,2)
+        draw_rock(screen,(x+59,y+25),2,2)
+        draw_rock(screen,(x+45,y+55),0,2)
     elif num==6:
-        draw_rock(screen,(x+10,y+27),0,1)
-        draw_rock(screen,(x+71,y+25),2,1)
-        draw_rock(screen,(x+11,y+7),1,2)
-        draw_rock(screen,(x+74,y+5),2,2)
-        draw_rock(screen,(x+68,y+47),0,2)
-        draw_rock(screen,(x+7,y+45),0,0)
+        draw_rock(screen,(x+12,y+32),0,1)
+        draw_rock(screen,(x+51,y+30),2,1)
+        draw_rock(screen,(x+31,y+17),1,2)
+        draw_rock(screen,(x+44,y+15),2,2)
+        draw_rock(screen,(x+68,y+37),0,2)
+        draw_rock(screen,(x+27,y+45),0,0)
     elif num>=7:
-        draw_rock(screen,(x+11,y+28),0,1,code=False)
-        draw_rock(screen,(x+12,y+8),2,1,code=False)
-        draw_rock(screen,(x+6,y+46),1,2,code=False)
-        draw_rock(screen,(x+66,y+48),2,2,code=False)
-        draw_rock(screen,(x+74,y+6),0,2,code=False)
-        draw_rock(screen,(x+73,y+26),0,0,code=False)
-        draw_rock(screen,(x+48,y+27),1,1,code=False)       
+        draw_rock(screen,(x+21,y+48),0,1)
+        draw_rock(screen,(x+12,y+30),2,1)
+        draw_rock(screen,(x+26,y+36),1,2)
+        draw_rock(screen,(x+56,y+48),2,2)
+        draw_rock(screen,(x+74,y+32),0,2)
+        draw_rock(screen,(x+73,y+26),0,0)
+        draw_rock(screen,(x+48,y+59),1,1)       
     
 def winner(screen,whowin):
-    screen.fill(earth_brown)
+    screen.fill(scenery)
     draw_grid(screen)
     option=""
     if whowin==1:
